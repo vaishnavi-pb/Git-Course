3e4486e72d68d8ae61b59526929fcafdc2d09cc9
vaishnavi <vaishnavipb2018@gmail.com>
2025-01-30 15:23:32 +0530

    my restful api

---
+++ basic-rest-api/src/test/java/com/example/basic_rest_api/BasicRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
